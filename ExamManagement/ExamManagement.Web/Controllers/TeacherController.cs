58039dd30d33d998699fed2a54ffa7ea4d6def94
Victor Manoliu <victormanoliu97@gmail.com>
2018-12-12 00:36:13 +0200

    Updated Teacher Controller to actually create an exam

--- ExamManagement/ExamManagement.Web/Controllers/TeacherController.cs
+++ ExamManagement/ExamManagement.Web/Controllers/TeacherController.cs
@@ -22,7 +22,7 @@ namespace ExamManagement.Web.Controllers
             _examRepository = examRepository;
         }
 
-        [HttpPost("create-exam")]
+        [HttpPost("CreateExam")]
         public HttpResponseMessage CreateExam(CreateExamRequest createExamRequest)
         {
             if(createExamRequest == null)
@@ -31,11 +31,12 @@ namespace ExamManagement.Web.Controllers
             }
             Exam tmpExam = new Exam(1, createExamRequest.CourseID, createExamRequest.ExamDate, createExamRequest.Room, createExamRequest.StartTime, createExamRequest.EndTime, createExamRequest.Type);
             _examRepository.CreateExam(tmpExam);
+
             return new HttpResponseMessage(System.Net.HttpStatusCode.OK);
         }
 
         
-        [HttpPost("start-exam")]
+        [HttpPost("StartExam")]
         public HttpResponseMessage StartExam(StartExamRequest startExamRequest)
         {
             if(startExamRequest == null)
@@ -45,7 +46,7 @@ namespace ExamManagement.Web.Controllers
             return new HttpResponseMessage(System.Net.HttpStatusCode.OK);
         }
 
-        [HttpPost("end-exam")]
+        [HttpPost("EndExam")]
         public HttpResponseMessage EndExam(EndExamRequest endExamRequest)
         {
             if(endExamRequest == null)
@@ -55,7 +56,7 @@ namespace ExamManagement.Web.Controllers
 
             return new HttpResponseMessage(System.Net.HttpStatusCode.OK);
         }
-        [HttpPost("set-grades")]
+        [HttpPost("SetGrades")]
         public HttpResponseMessage SetGrades(SetGradeRequest setGradeRequest)
         {
             if (setGradeRequest == null)
@@ -64,7 +65,7 @@ namespace ExamManagement.Web.Controllers
             }
             return new HttpResponseMessage(System.Net.HttpStatusCode.OK);
         }
-        [HttpPost("publish-grades")]
+        [HttpPost("PublishGrades")]
         public HttpResponseMessage PublishGrades(SetGradeRequest setGradeRequest)
         {
             if (setGradeRequest == null)
