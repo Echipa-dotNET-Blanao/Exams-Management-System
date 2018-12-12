05d7a75d92e5c4a381bbd1ab94ff67e389e5294c
Paius Emanuel Andrei <pandaismyname1@gmail.com>
2018-12-12 02:31:21 +0200

    added Mail support

---
+++ ExamManagement/ExamManagement.Core/Services/MailService.cs
@@ -0,0 +1,40 @@
+﻿using MailKit.Net.Smtp;
+using MimeKit;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExamManagement.Core.Services
+{
+    class MailService
+    {
+        public static void SendEmail(string toAdress, string subject, string body)
+        {
+            string FromAddress = "fii.exam.management@gmail.com";
+
+            string SmtpServer = "smtp.gmail.com";
+            int SmtpPortNumber = 587;
+
+            var mimeMessage = new MimeMessage();
+            mimeMessage.From.Add(new MailboxAddress(FromAddress));
+            mimeMessage.To.Add(new MailboxAddress(toAdress));
+            mimeMessage.Subject = subject;
+            mimeMessage.Body = new TextPart("plain")
+            {
+                Text = body
+            };
+
+            using (var client = new SmtpClient())
+            {
+
+                client.Connect(SmtpServer, SmtpPortNumber, false);
+                client.Authenticate("fii.exam.management@gmail.com", "fiiexam123");
+                client.Send(mimeMessage);
+                Console.WriteLine("The mail has been sent successfully !!");
+                Console.ReadLine();
+                client.Disconnect(true);
+
+            }
+        }
+    }
+}
