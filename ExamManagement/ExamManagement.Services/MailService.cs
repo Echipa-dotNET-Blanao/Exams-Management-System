09ff92feebe299becd9e0b969227369a137bbde0
Paius Emanuel Andrei <pandaismyname1@gmail.com>
2018-12-14 23:19:11 +0200

    created a new Services Layer for Onion

---
+++ ExamManagement/ExamManagement.Services/MailService.cs
@@ -0,0 +1,35 @@
+﻿using MailKit.Net.Smtp;
+using MimeKit;
+
+namespace ExamManagement.Services
+{
+    public static class MailService
+    {
+        public static void SendEmail(string toAdress, string subject, string body)
+        {
+            string FromAddress = "fii.exam.management@gmail.com";
+
+            string SmtpServer = "smtp.gmail.com";
+            int SmtpPortNumber = 587;
+
+            var mimeMessage = new MimeMessage();
+            mimeMessage.From.Add(new MailboxAddress(FromAddress));
+            mimeMessage.To.Add(new MailboxAddress(toAdress));
+            mimeMessage.Subject = subject;
+            mimeMessage.Body = new TextPart("plain")
+            {
+                Text = body
+            };
+
+            using (var client = new SmtpClient())
+            {
+
+                client.Connect(SmtpServer, SmtpPortNumber, false);
+                client.Authenticate("fii.exam.management@gmail.com", "fiiexam123");
+                client.Send(mimeMessage);
+                client.Disconnect(true);
+
+            }
+        }
+    }
+}
