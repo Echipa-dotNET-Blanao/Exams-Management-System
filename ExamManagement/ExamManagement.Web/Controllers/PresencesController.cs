66a4a7f0a9da35da89b1bca7dae43d39e885bcef
razvan9921 <razvangabriel9921@gmail.com>
2018-12-15 15:36:55 +0200

    Refactor the presenceService and presenceController

--- ExamManagement/ExamManagement.Web/Controllers/PresencesController.cs
+++ ExamManagement/ExamManagement.Web/Controllers/PresencesController.cs
@@ -1,7 +1,5 @@
-﻿using ExamManagement.Core.Interfaces.Repositories;
-using ExamManagement.Web.Requests;
+﻿using ExamManagement.Web.Requests;
 using Microsoft.AspNetCore.Mvc;
-using System;
 using System.Net;
 using System.Net.Http;
 using ExamManagement.Core.Interfaces.Services;
@@ -13,24 +11,17 @@ namespace ExamManagement.Web.Controllers
     public class PresencesController : ControllerBase
     {
         private readonly IPresenceService _presenceService;
+
         public PresencesController(IPresenceService presenceService)
         {
             _presenceService = presenceService;
         }
 
-        //TODO : In controllers will never be logic like defensive codding. Move it to repo or where you have the logic of the app.
-        //TODO : Also respect naming convensions for the routes and the variables.
         [HttpPut]
         public HttpResponseMessage Presence([FromBody] MarkPresenceRequest markPresenceRequest)
         {
-            if (markPresenceRequest == null) throw new ArgumentNullException(nameof(markPresenceRequest));
-
-            if (markPresenceRequest.StudentID != null && markPresenceRequest.ExamID != 0 && markPresenceRequest.Token != null)
-            {
-                _presenceService.MarkStudentPresent(markPresenceRequest.StudentID, markPresenceRequest.ExamID, markPresenceRequest.Token);
-                return new HttpResponseMessage(HttpStatusCode.Accepted);
-            }
-            return new HttpResponseMessage(HttpStatusCode.GatewayTimeout);
-        }
+            _presenceService.MarkStudentPresent(markPresenceRequest.StudentID, markPresenceRequest.ExamID, markPresenceRequest.Token);
+            return new HttpResponseMessage(HttpStatusCode.Accepted);
+       }
     }
 }
