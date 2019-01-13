5e111a19beefd66613818706d68382b4f1f780d8
Victor Manoliu <victormanoliu97@gmail.com>
2019-01-14 01:44:29 +0200

    Improvments on Auth Service

--- ExamManagement/ExamManagement.Web/WebApp/ExamGradeManagement/src/app/login/auth/auth.component.ts
+++ ExamManagement/ExamManagement.Web/WebApp/ExamGradeManagement/src/app/login/auth/auth.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { LoginService } from '../../services/login.service';
 
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.component.html',
-  styleUrls: ['./auth.component.scss']
+  styleUrls: ['./auth.component.scss'],
+  providers : [LoginService]
 })
 export class AuthComponent implements OnInit {
 
-  constructor() { }
+  constructor(private loginService : LoginService) { }
 
   ngOnInit() {
   }
 
+  makePostRequest(userName : string, password : string) {
+    this.loginService.makeLogin(userName, password);
+  }
 }
