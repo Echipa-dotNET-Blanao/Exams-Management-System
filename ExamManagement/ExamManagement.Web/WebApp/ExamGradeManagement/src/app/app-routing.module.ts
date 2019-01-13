0ff0ea5d87fe9136c090b3138a5c5c1c2c6678b9
Hurbea, Razvan <Razvan.Hurbea@centric.eu>
2019-01-13 22:55:39 +0200

    solved some other issues

--- ExamManagement/ExamManagement.Web/WebApp/ExamGradeManagement/src/app/app-routing.module.ts
+++ ExamManagement/ExamManagement.Web/WebApp/ExamGradeManagement/src/app/app-routing.module.ts
@@ -2,11 +2,26 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { AuthComponent } from './login/auth/auth.component';
+import { StudentDashboardComponent } from './student/student-dashboard/student-dashboard.component';
+import { DashboardComponent } from './teacher/dashboard/dashboard.component';
+import { TeacherGradeExamComponent } from './teacher/teacher-grade-exam/teacher-grade-exam.component';
 
 const routes: Routes = [
   {
     path: 'login',
     component: AuthComponent
+  },
+  {
+    path: 'student',
+    component: StudentDashboardComponent
+  },
+  {
+    path: 'teacher-dashboard',
+    component: DashboardComponent
+  },
+  {
+    path: 'teacher-grades',
+    component: TeacherGradeExamComponent
   },
   {
     path: '**',
