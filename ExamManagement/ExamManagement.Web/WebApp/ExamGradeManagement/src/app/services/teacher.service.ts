0e698e94f8b7d45da616e89043b43851035f4b87
Prodan Marius <mprodan27@gmail.com>
2019-01-16 01:08:24 +0200

    Some Additions

--- ExamManagement/ExamManagement.Web/WebApp/ExamGradeManagement/src/app/services/teacher.service.ts
+++ ExamManagement/ExamManagement.Web/WebApp/ExamGradeManagement/src/app/services/teacher.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TeacherService {
 
-  constructor() { }
+  teacherUrl : string = 'https://localhost:44390/Exams/Teachers';
+
+  constructor(private http: HttpClient) { }
+
+  async getTeacherInformation(userId:string): Promise<any>  {
+    return this.http.get(this.teacherUrl+`?TeacherId=${userId}`).toPromise<any>();
+  }
 }
--- ExamManagement/ExamManagement.Web/WebApp/ExamGradeManagement/src/app/teacher/dashboard/dashboard.component.ts
+++ ExamManagement/ExamManagement.Web/WebApp/ExamGradeManagement/src/app/teacher/dashboard/dashboard.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { TeacherService } from 'src/app/services/teacher.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss']
+  styleUrls: ['./dashboard.component.scss'],
+  providers: [TeacherService]
 })
 export class DashboardComponent implements OnInit {
-
-  constructor() { }
+  constructor(private teacherService : TeacherService,private router: Router) { }
 
   ngOnInit() {
   }
-
+  async logOut(){
+    this.router.navigate(['/login']);
+  }
+  async showTeacherName(){
+    
+  }
 }
